b171af10e96b3936af8dc2575bed76b483826422
front-dm <fresh.freedoms@gmail.com>
2017-03-25 18:35:55 +0300

    Update JS

--- js/common.js
+++ js/common.js
@@ -1,8 +1,25 @@
 (function (argument) {
+	//PORTFOLIO LINK
 	$('.item-port').click(function(event) {
 		var linkTo = $(this).data('url');
 		var currentLink = window.location.hostname;
-		alert(currentLink + "/template/" + linkTo + "/index.html")
+		window.location.href = currentLink + "/template/" + linkTo + "/index.html";
 	});
-	var a = $('.item-port img').data('url');
+	
+	//NAVIGATION
+	var itemsMenu = $('.items-menu li');
+
+	$(itemsMenu).click(function(event) {
+	//ADD ACTIVE CLASS
+		for (var i = 0; i < itemsMenu.length; i++) {
+			$(itemsMenu[i]).removeClass('active');
+		};
+		$(this).addClass('active');
+	
+	//LINK TO ANCHOR
+		var hrefMenu = '#' + $(this).data('url');
+		$('html,body').animate({scrollTop: $(hrefMenu).offset().top}, 500);
+	});
+
+
 })()
